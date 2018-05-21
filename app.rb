2ba39b3dd54b92dcd98e261450d956fb82c0824a
Vanessa Toro <vanetoro84@gmail.com>
2018-05-21 22:13:49 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -21,7 +21,7 @@ class App < Sinatra::Base
   get '/add_numbers/:num1/:num2' do
     @num1 = params[:num1]
     @num2 = params[:num2]
-    <%= @num * @num %>
+    "<%= @num1 * @num2 %>"
     
 
   end
